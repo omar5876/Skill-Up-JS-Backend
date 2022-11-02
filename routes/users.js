d5b957d9fcac9cd7e3f84ef5b967346c006a0610
Guillermo <guillef33@gmail.com>
2022-11-02 11:56:01 -0300

    add route /:id in router

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const { get, post, put } = require('../controllers/users');
+const express = require("express");
+const { get, post, put } = require("../controllers/users");
 
 const router = express.Router();
 
-router.get('/', get);
-router.post('/', post);
-router.put('/:id', put);
+router.get("/", get);
+router.get("/:id", get);
+router.post("/", post);
+router.put("/:id", put);
 
 module.exports = router;
