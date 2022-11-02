823cafa16dfb27f9fb921cf1cff9e2ec37cda121
martinrotela <martinrotela47@gmail.com>
2022-11-02 04:44:14 -0300

    feat: put user

--- controllers/users.js
+++ controllers/users.js
@@ -22,7 +22,6 @@ module.exports = {
             next(httpError);
         }
     }),
-
     post: catchAsync(async (req, res, next) => {
         const { body } = req;
         try {
@@ -50,6 +49,38 @@ module.exports = {
             const httpError = createHttpError(
                 error.statusCode,
                 `[Error creating user] - [index - POST]: ${error.message}`
+            );
+            next(httpError);
+        }
+    }),
+    put: catchAsync(async (req, res, next) => {
+        const { body } = req;
+        const { id } = req.params;
+        try {
+            const user = await User.findByPk(id);
+            if (!user) {
+                const httpError = createHttpError(404, 'User does not exist');
+                next(httpError);
+            }
+
+            const newUser = { ...body };
+
+            if (newUser.password) {
+                const salt = bcrypt.genSaltSync();
+                newUser.password = bcrypt.hashSync(body.password, salt);
+            }
+            const response = await user.update(newUser);
+
+            console.log(response);
+            endpointResponse({
+                res,
+                message: 'User updated successfuly',
+                body: response,
+            });
+        } catch (error) {
+            const httpError = createHttpError(
+                error.statusCode,
+                `[Error updating user] - [index - PUT]: ${error.message}`
             );
             next(httpError);
         }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { get, post } = require('../controllers/users');
+const { get, post, put } = require('../controllers/users');
 
 const router = express.Router();
 
 router.get('/', get);
 router.post('/', post);
+router.put('/:id', put);
 
 module.exports = router;
