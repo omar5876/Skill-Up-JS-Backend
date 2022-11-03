0c3505fef939588c78324b25c394c4789c66bbe1
martinrotela <martinrotela47@gmail.com>
2022-11-03 00:50:00 -0300

    feat: get categories

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,30 +1,47 @@
-const createHttpError = require('http-errors')
-const { Category } = require('../database/models')
-const { endpointResponse } = require('../helpers/success')
-const { catchAsync } = require('../helpers/catchAsync')
+const createHttpError = require('http-errors');
+const { Category } = require('../database/models');
+const { endpointResponse } = require('../helpers/success');
+const { catchAsync } = require('../helpers/catchAsync');
 
 module.exports = {
-	post: catchAsync(async (req, res, next) => {
-		try {
-			const { name, description } = req.body
+    post: catchAsync(async (req, res, next) => {
+        try {
+            const { name, description } = req.body;
 
-			const response = await Category.create({
-				name,
-				description,
-			})
+            const response = await Category.create({
+                name,
+                description,
+            });
 
-			endpointResponse({
-				res,
-				code: 201,
-				message: 'Category created successfully',
-				body: response,
-			})
-		} catch {
-			const httpError = createHttpError(
-				error.statusCode,
-				`[Error to insert category] - [${req.path} - POST]: ${error.message}`
-			)
-			next(httpError)
-		}
-	}),
-}
+            endpointResponse({
+                res,
+                code: 201,
+                message: 'Category created successfully',
+                body: response,
+            });
+        } catch {
+            const httpError = createHttpError(
+                error.statusCode,
+                `[Error to insert category] - [${req.path} - POST]: ${error.message}`
+            );
+            next(httpError);
+        }
+    }),
+
+    get: catchAsync(async (req, res, next) => {
+        try {
+            const response = await Category.findAll();
+            endpointResponse({
+                res,
+                message: 'Categories retrieved successfuly',
+                body: response,
+            });
+        } catch (error) {
+            const httpError = createHttpError(
+                error.statusCode,
+                `[Error to insert category] - [${req.path} - POST]: ${error.message}`
+            );
+            next(httpError);
+        }
+    }),
+};
--- routes/categories.js
+++ routes/categories.js
@@ -1,8 +1,9 @@
-const { Router } = require('express')
-const { post } = require('../controllers/categories')
+const { Router } = require('express');
+const { post, get } = require('../controllers/categories');
 
-const router = Router()
+const router = Router();
 
-router.post('/', post)
+router.post('/', post);
+router.get('/', get);
 
-module.exports = router
+module.exports = router;
