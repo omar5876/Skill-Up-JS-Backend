5323252be94f08f043e029eddfde91133282df01
martinrotela <martinrotela47@gmail.com>
2022-11-03 01:38:49 -0300

    fix: category transactions error

--- database/models/transaction.js
+++ database/models/transaction.js
@@ -1,29 +1,32 @@
-"use strict";
-const { Model } = require("sequelize");
+'use strict';
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Transaction extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      Transaction.belongsTo(models.User, { foreignKey: "userId" });
+    class Transaction extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            Transaction.belongsTo(models.User, { foreignKey: 'userId' });
+            Transaction.belongsTo(models.Category, {
+                foreignKey: 'categoryId',
+            });
+        }
     }
-  }
-  Transaction.init(
-    {
-      description: DataTypes.STRING,
-      amount: DataTypes.DECIMAL,
-      userId: DataTypes.INTEGER,
-      date: DataTypes.DATE,
-    },
-    {
-      sequelize,
-      paranoid: true,
-      modelName: "Transaction",
-    }
-  );
-  return Transaction;
+    Transaction.init(
+        {
+            description: DataTypes.STRING,
+            amount: DataTypes.DECIMAL,
+            userId: DataTypes.INTEGER,
+            date: DataTypes.DATE,
+        },
+        {
+            sequelize,
+            paranoid: true,
+            modelName: 'Transaction',
+        }
+    );
+    return Transaction;
 };
