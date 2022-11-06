270503441460c0ba063f639fdabe813705cd105d
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2022-11-05 20:54:15 -1100

    fix resolved id not found and endpointResponse

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,99 +1,100 @@
-const createHttpError = require('http-errors');
-const { Category } = require('../database/models');
-const { endpointResponse } = require('../helpers/success');
-const { catchAsync } = require('../helpers/catchAsync');
+const createHttpError = require("http-errors");
+const { Category } = require("../database/models");
+const { endpointResponse } = require("../helpers/success");
+const { catchAsync } = require("../helpers/catchAsync");
 
 module.exports = {
-    post: catchAsync(async (req, res, next) => {
-        try {
-            const { name, description } = req.body;
+  post: catchAsync(async (req, res, next) => {
+    try {
+      const { name, description } = req.body;
 
-            const response = await Category.create({
-                name,
-                description,
-            });
+      const response = await Category.create({
+        name,
+        description,
+      });
 
-            endpointResponse({
-                res,
-                code: 201,
-                message: 'Category created successfully',
-                body: response,
-            });
-        } catch {
-            const httpError = createHttpError(
-                error.statusCode,
-                `[Error to insert category] - [${req.path} - POST]: ${error.message}`
-            );
-            next(httpError);
-        }
-    }),
+      endpointResponse({
+        res,
+        code: 201,
+        message: "Category created successfully",
+        body: response,
+      });
+    } catch {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error to insert category] - [${req.path} - POST]: ${error.message}`
+      );
+      next(httpError);
+    }
+  }),
 
-    get: catchAsync(async (req, res, next) => {
-        try {
-            const response = await Category.findAll();
-            endpointResponse({
-                res,
-                message: 'Categories retrieved successfuly',
-                body: response,
-            });
-        } catch (error) {
-            const httpError = createHttpError(
-                error.statusCode,
-                `[Error to insert category] - [${req.path} - POST]: ${error.message}`
-            );
-            next(httpError);
-        }
-    }),
+  get: catchAsync(async (req, res, next) => {
+    try {
+      const response = await Category.findAll();
+      endpointResponse({
+        res,
+        message: "Categories retrieved successfuly",
+        body: response,
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error to insert category] - [${req.path} - POST]: ${error.message}`
+      );
+      next(httpError);
+    }
+  }),
 
-    deleteCategory: catchAsync(async (req, res, next) => {
-        const { id } = req.params;
-        try {
-            const category = await Category.findByPk(id);
-            if (!category) {
-                const httpError = createHttpError(404, 'category does not exist');
-                return next(httpError);
-            }
-            category.destroy();
-            endpointResponse({
-                res,
-                message: 'Category deleted',
-            });
-        } catch (error) {
-            const httpError = createHttpError(
-                error.statusCode,
-                `[Error deleting user] - [index - DELETE]: ${error.message}`
-            );
-            next(httpError);
-        }
-    }),
+  deleteCategory: catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const category = await Category.findByPk(id);
+      if (!category) {
+        const httpError = createHttpError(404, "category does not exist");
+        return next(httpError);
+      }
+      category.destroy();
+      endpointResponse({
+        res,
+        message: "Category deleted",
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error deleting user] - [index - DELETE]: ${error.message}`
+      );
+      next(httpError);
+    }
+  }),
 
-    updateCategory: catchAsync(async (req, res, next) => {
-        const { id } = req.params
-        const { name, description } = req.body
+  updateCategory: catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const { name, description } = req.body;
 
-        const category = await Category.findByPk(id)
-        if (!Category) {
-            return res.status(400).json({
-                ok: false,
-                msg: 'The category was not found.',
-            })
-        }
-        category.name = name
-        category.description = description
-        await category
-            .save()
-            .then((updatedCategory) => res.status(201).json({
-                ok: true,
-                msg: 'Category updated successfully',
-                result: { category: { ...updatedCategory } },
-            }))
-            .catch((err) => {
-                const httpError = createHttpError(
-                    err.statusCode,
-                    `[Error updating Categories] - [index - PUT]: ${err.message}`,
-                )
-                next(httpError)
-            })
-    }),
+    const category = await Category.findByPk(id);
 
-}
+    if (!category) {
+      const httpError = createHttpError(404, "The category was not found.");
+      return next(httpError);
+    }
+    category.name = name;
+    category.description = description;
+    await category
+      .save()
+      .then((updatedCategory) =>
+        endpointResponse({
+          res,
+          message: "Category updated successfully",
+          body: response,
+          options: { category: { ...updatedCategory } },
+        })
+      )
+      .catch((err) => {
+        const httpError = createHttpError(
+          err.statusCode,
+          `[Error updating Categories] - [index - PUT]: ${err.message}`
+        );
+        next(httpError);
+      });
+  }),
+};
