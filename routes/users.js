24bf5778d969d0760c8ac7a418dfe51b3f02a8d7
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2022-11-10 19:28:25 -1100

    fix:correccion de bug

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,14 @@ const express = require("express");
 const { get, post, put, getById, del } = require("../controllers/users");
 const {
   isAuthenticated,
-  hasAuthenticatedRol,
+hasOwnershipRol,
 } = require("../middlewares/authJwt");
 const router = express.Router();
 
-router.get("/", get);
-router.get("/:id", getById);
+router.get("/",isAuthenticated,hasOwnershipRol, get);
+router.get("/:id",isAuthenticated,hasOwnershipRol, getById);
 router.post("/", post);
-router.put("/:id", isAuthenticated, hasAuthenticatedRol(["admin"]), put);
-router.delete("/:id", isAuthenticated, hasAuthenticatedRol(["admin"]), del);
+router.put("/:id", isAuthenticated,hasOwnershipRol, put);
+router.delete("/:id", isAuthenticated, hasOwnershipRol, del);
 
 module.exports = router;
