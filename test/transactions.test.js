8a0ac1cf771be0fd345ed38681129e3be0ef6cff
axel rueda <axelcuer@gmail.com>
2022-11-11 14:56:03 -0300

    adding test to edit and delete transactions endpoints

--- test/transactions.test.js
+++ test/transactions.test.js
@@ -7,7 +7,7 @@ const factory = require('./utils/factories')
 let transactions
 
 beforeEach(async () => {
-	await cleanDB
+	await cleanDB()
 	transactions = await factory.transctionsFactory(2)
 })
 
@@ -35,7 +35,6 @@ describe('get transaction', () => {
 describe('creating a transaction', () => {
 	test('when post a valid transaction, is added to the table', async () => {
 		const transaction = factory.generateRandomTransaction()
-		console.log(transaction)
 		const response = await API.post('/transactions/').send(transaction).expect(201)
 		expect(response.body.body.amount).toBe(transaction.amount)
 	})
@@ -65,13 +64,46 @@ describe('creating a transaction', () => {
 })
 
 describe('editing transactions', () => {
-	test('when existing a transaction and send correct data, is edited', async () => {})
-	test('when existing a transaction and send missing data, receive an error', async () => {})
-	test('when the transaction non-existent, receive an error', async () => {})
+	test('when existing a transaction and send correct data, is edited', async () => {
+		const transaction = { ...transactions[0], amount: transactions[0].amount - 50 }
+		const response = await API.put(`/transactions/${transaction.id}`).send(transaction).expect(200)
+		const body = response.body.body
+		expect(body.id).toBe(transaction.id)
+		expect(body.amount).toBe(transaction.amount)
+		expect(body.userId).toBe(transaction.userId)
+	})
+	test('when existing a transaction and send correct data, the size of the table dont change', async () => {
+		const transaction = { ...transactions[0], amount: transactions[0].amount - 50 }
+		await API.put(`/transactions/${transaction.id}`).send(transaction).expect(200)
+		const response = await API.get('/transactions').expect(200)
+		const body = response.body.body
+		expect(body).toHaveLength(transactions.length)
+	})
+
+	// ~~~~ in the requirements ask to validate that a user, category, quantity and date be sent ~~~~~
+	test('when existing a transaction and send missing data, receive an error', async () => {
+		const transaction = { ...transactions[0], amount: undefined }
+		const response = await API.put(`/transactions/${transaction.id}`).send(transaction).expect(400)
+		expect(response.type).toBe('text/html')
+	})
+	test('when the transaction non-existent, receive an error', async () => {
+		const transaction = { ...transactions[0], amount: transactions[0].amount - 50 }
+		const response = await API.put(`/transactions/50`).send(transaction).expect(400)
+		expect(response.type).toBe('text/html')
+	})
 })
 
 describe('deleting transactions', () => {
-	test('when existing a transaction, is deleted', async () => {})
-	test('when existing a transaction the size of the transactions table is reduced', async () => {})
-	test('when the transaction non-existent, receive an error', async () => {})
+	test('when existing a transaction, is deleted', async () => {
+		await API.delete(`/transactions/${transactions[0].id}`).expect(200)
+	})
+	test('when existing a transaction the size of the transactions table is reduced', async () => {
+		await API.delete(`/transactions/${transactions[0].id}`).expect(200)
+		const response = await API.get('/transactions').expect(200)
+		expect(response.body.body).toHaveLength(transactions.length - 1)
+	})
+	test('when the transaction non-existent, receive an error', async () => {
+		const response = await API.delete(`/transactions/-25`).expect(404)
+		expect(response.type).toBe('text/html')
+	})
 })
