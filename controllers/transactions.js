4d1df105776246359469eda960dd97fcefc7ca57
renatogastonruizgithub <renatogastonruiz.00@gmail.com>
2022-11-12 12:46:23 -1100

    fix-balance user

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,150 +1,217 @@
-const createHttpError = require('http-errors')
-const { Transaction,User,Category } = require('../database/models')
-const { endpointResponse } = require('../helpers/success')
-const { catchAsync } = require('../helpers/catchAsync')
-
+const createHttpError = require("http-errors");
+const { Transaction, User, Category } = require("../database/models");
+const { endpointResponse } = require("../helpers/success");
+const { catchAsync } = require("../helpers/catchAsync");
+const sequelize = require("sequelize");
+const { where } = require("sequelize");
 // example of a controller. First call the service, then build the controller method
 module.exports = {
   get: catchAsync(async (req, res, next) => {
     try {
       const response = await Transaction.findAll({
-        attributes: {          
-        exclude: [
-                  "deletedAt",
-                  "userId",
-                  "categoryId","updatedAt"
-                ],
-              }, 
-        include:
-          [{
-            model: User, attributes:["firstName"]
+        attributes: {
+          exclude: ["deletedAt", "userId", "categoryId", "updatedAt"],
+        },
+        include: [
+          {
+            model: User,
+            attributes: ["firstName"],
           },
-          
-          { model: Category, attributes: ['name'] }
-          ]
+
+          { model: Category, attributes: ["name"] },
+        ],
       });
-      if(!response.length) return next(createHttpError(404, 'There are no transactions'))
+      if (!response.length)
+        return next(createHttpError(404, "There are no transactions"));
       endpointResponse({
         res,
-        message: 'Transactions retrieved successfully',
+        message: "Transactions retrieved successfully",
         body: response,
-      })
+      });
     } catch (error) {
       const httpError = createHttpError(
         error.statusCode,
-        `[Error retrieving Transactions] - [index - GET]: ${error.message}`,
-      )
-      next(httpError)
+        `[Error retrieving Transactions] - [index - GET]: ${error.message}`
+      );
+      next(httpError);
     }
   }),
 
   getById: catchAsync(async (req, res, next) => {
-    const {id} = req.params
+    const { id } = req.params;
     try {
       const response = await Transaction.findOne({
         where: { id: id },
-           attributes: {          
-        exclude: [
-                  "deletedAt",
-                  "userId",
-                  "categoryId","updatedAt"
-                ],
-              }, 
-        include:
-          [{
-            model: User, attributes:["firstName"]
+        attributes: {
+          exclude: ["deletedAt", "userId", "categoryId", "updatedAt"],
+        },
+        include: [
+          {
+            model: User,
+            attributes: ["firstName"],
           },
-          
-          { model: Category, attributes: ['name'] }
-          ]
-      
-      })
-      if(!response) return next(createHttpError(404, 'Transaction not found'));
+
+          { model: Category, attributes: ["name"] },
+        ],
+      });
+      if (!response) return next(createHttpError(404, "Transaction not found"));
       endpointResponse({
         res,
-        message: 'Transaction retrieved successfully',
+        message: "Transaction retrieved successfully",
         body: response,
-      })
+      });
     } catch (error) {
       const httpError = createHttpError(
         error.statusCode,
-        `[Error retrieving Transaction] - [index - GET]: ${error.message}`,
-      )
-      next(httpError)
+        `[Error retrieving Transaction] - [index - GET]: ${error.message}`
+      );
+      next(httpError);
     }
   }),
 
   createTransaction: catchAsync(async (req, res, next) => {
     try {
-      const response = await Transaction.create(req.body)
-      if(!response) return next(createHttpError(500, 'Transaction hasn\'t been created'));
+      const response = await Transaction.create(req.body);
+      if (!response)
+        return next(createHttpError(500, "Transaction hasn't been created"));
       endpointResponse({
         res,
-        message: 'Transaction was created successfully',
+        message: "Transaction was created successfully",
         body: response,
-      })
+      });
     } catch (error) {
       const httpError = createHttpError(
         error.statusCode,
-        `[Error creating Transaction] - [index - POST]: ${error.message}`,
-      )
-      next(httpError)
+        `[Error creating Transaction] - [index - POST]: ${error.message}`
+      );
+      next(httpError);
     }
   }),
 
   updateTransaction: catchAsync(async (req, res, next) => {
     let transactionFound;
     try {
-        transactionFound = await Transaction.findByPk(req.params.id)
-        if(!transactionFound) return next(createHttpError(404, 'Transaction not found'));
-    } catch (error) {
-        const httpError = createHttpError(
-            error.statusCode,
-            `[Error finding Transaction] - [index - PUT]: ${error.message}`,
-          )
-          next(httpError)
-    }
-    try {
-      const response = await transactionFound.update(req.body)
-      endpointResponse({
-        res,
-        message: 'Transaction was updated successfully',
-        body: response,
-      })
+      transactionFound = await Transaction.findByPk(req.params.id);
+      if (!transactionFound)
+        return next(createHttpError(404, "Transaction not found"));
     } catch (error) {
       const httpError = createHttpError(
         error.statusCode,
-        `[Error updating Transaction] - [index - PUT]: ${error.message}`,
-      )
-      next(httpError)
+        `[Error finding Transaction] - [index - PUT]: ${error.message}`
+      );
+      next(httpError);
+    }
+    try {
+      const response = await transactionFound.update(req.body);
+      endpointResponse({
+        res,
+        message: "Transaction was updated successfully",
+        body: response,
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error updating Transaction] - [index - PUT]: ${error.message}`
+      );
+      next(httpError);
     }
   }),
 
   deleteTransaction: catchAsync(async (req, res, next) => {
     let transactionFound;
     try {
-        transactionFound = await Transaction.findByPk(req.params.id)
-        if(!transactionFound) return next(createHttpError(404, 'Transaction not found'));
-    } catch (error) {
-        const httpError = createHttpError(
-            error.statusCode,
-            `[Error finding Transaction] - [index - DELETE]: ${error.message}`,
-          )
-          next(httpError)
-    }
-    try {
-      const response = await transactionFound.destroy(req.params.id)
-      endpointResponse({
-        res,
-        message: 'Transaction was deleted successfully',
-        body: response,
-      })
+      transactionFound = await Transaction.findByPk(req.params.id);
+      if (!transactionFound)
+        return next(createHttpError(404, "Transaction not found"));
     } catch (error) {
       const httpError = createHttpError(
         error.statusCode,
-        `[Error deleting Transaction] - [index - DELETE]: ${error.message}`,
-      )
-      next(httpError)
+        `[Error finding Transaction] - [index - DELETE]: ${error.message}`
+      );
+      next(httpError);
+    }
+    try {
+      const response = await transactionFound.destroy(req.params.id);
+      endpointResponse({
+        res,
+        message: "Transaction was deleted successfully",
+        body: response,
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error deleting Transaction] - [index - DELETE]: ${error.message}`
+      );
+      next(httpError);
     }
   }),
-}
+
+  balanceByUser: catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const errorIdUser = await Transaction.findByPk(id);
+    if (!errorIdUser) return next(createHttpError(404, "user no existe"));
+
+    try {
+      const outcomesData = await Transaction.findAll({
+        where: { userId: id },
+        attributes: [
+          [sequelize.fn("sum", sequelize.col("amount")), "totalOutcomes"],
+        ],
+        include: [
+          {
+            model: User,
+            attributes: ["firstName", "id"],
+          },
+          {
+            model: Category,
+            where: { name: "OUTCOMES" },
+            attributes: ["name"],
+          },
+        ],
+        raw: true,
+      });
+      const incomesData = await Transaction.findAll({
+        where: { userId: id },
+        attributes: [
+          [sequelize.fn("sum", sequelize.col("amount")), "totalIncomes"],
+        ],
+        include: [
+          {
+            model: User,
+            attributes: ["firstName", "id"],
+          },
+          {
+            model: Category,
+            where: { name: "INCOMES" },
+            attributes: ["name"],
+          },
+        ],
+      });
+
+      const outcomes = [];
+      const incomes = [];
+      incomesData.forEach((key) => {
+        incomes.push(key.dataValues.totalIncomes);
+        return incomes.toString();
+      });
+
+      outcomesData.forEach((key) => {
+        outcomes.push(key.totalOutcomes);
+        return outcomes.toString();
+      });
+      const balance = incomes - outcomes;
+
+      endpointResponse({
+        res,
+        message: "Transaction retrieved successfully",
+        body: { balance: balance },
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error retrieving Transaction] - [index - GET]: ${error.message}`
+      );
+      next(httpError);
+    }
+  }),
+};
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,19 +1,20 @@
-const express = require('express')
+const express = require("express");
 const {
-   get,
-   getById,
-   createTransaction,
-   updateTransaction,
-   deleteTransaction
-} = require('../controllers/transactions')
+  get,
+  getById,
+  balanceByUser,
+  createTransaction,
+  updateTransaction,
+  deleteTransaction,
+} = require("../controllers/transactions");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', get)
-router.get('/:id', getById)
-router.post('/', createTransaction)
-router.put('/:id', updateTransaction)
-router.delete('/:id', deleteTransaction)
+router.get("/", get);
+router.get("/:id", getById);
+router.get("/balance/:id", balanceByUser);
+router.post("/", createTransaction);
+router.put("/:id", updateTransaction);
+router.delete("/:id", deleteTransaction);
 
-
-module.exports = router
+module.exports = router;
